4993a39a7734db62ee09ebc7af685199a83c679c
Jade Gonzales <jadegonzales@Jades-MacBook-Pro.local>
2018-05-03 17:15:58 -0500

    2nd commit

---
+++ app/data/friends.js
@@ -0,0 +1,150 @@
+// These are the friend you could match with: 
+var friends = [
+    {
+      "name": "Pam Beesly",
+      "photo": "https://ia.media-imdb.com/images/M/MV5BOTUyZjA5ZTEtMjhkMi00MjFjLThjODEtYjVmMWVlZDEzZGQzXkEyXkFqcGdeQXVyNTM3MDMyMDQ@._V1_.jpg",
+      "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      "name": "Dwight Schrute",
+      "photo": "https://pbs.twimg.com/profile_images/1863312401/Dwight.jpg",
+      "scores": [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      "name": "Jim Halpert",
+      "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2017-08/9/11/asset/buzzfeed-prod-fastlane-01/sub-buzz-22381-1502291405-3.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+      "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      "name": "Michael Scott",
+      "photo": "https://pbs.twimg.com/profile_images/563105554822737921/F-iH3BMT_400x400.png",
+      "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      "name": "Creed Bratton",
+      "photo": "https://articlebio.com/uploads/bio/2018/01/16/creed-bratton.jpg",
+      "scores": [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      "name": "Kelly Kapoor",
+      "photo": "https://media1.popsugar-assets.com/files/thumbor/eXDFklp6xdWXLyvn9BcH5p-b_rk/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/06/21/951/n/1922283/05e3e789_edit_img_image_41730517_1466542783_kelly-th/i/Kelly-Kapoor-GIFs-From-Office.jpg",
+      "scores": [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+      "name": "Angela Martin",
+      "photo": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-office-angela-kinsey.jpg",
+      "scores": [
+        "3",
+        "2",
+        "4",
+        "2",
+        "2",
+        "4",
+        "4",
+        "3",
+        "3",
+        "2"
+      ]
+    },
+    {
+      "name": "Phyllis Vance",
+      "photo": "https://i.pinimg.com/originals/82/bf/fa/82bffa5496990acdec075f5a3305f6e1.jpg",
+      "scores": [
+        "5",
+        "2",
+        "1",
+        "3",
+        "3",
+        "1",
+        "4",
+        "2",
+        "4",
+        "3"
+      ]
+    },
+    {
+      "name": "Andy Bernard",
+      "photo": "https://3.bp.blogspot.com/-0AJRIjGjDas/T_pUMswIbII/AAAAAAAAF1Q/I5coBG2-dkA/s1600/andybernard.jpg",
+      "scores": [
+        "5",
+        "2",
+        "1",
+        "3",
+        "3",
+        "5",
+        "4",
+        "2",
+        "4",
+        "3"
+      ]
+    }
+  ]
+
+  module.exports = friends;
+  
